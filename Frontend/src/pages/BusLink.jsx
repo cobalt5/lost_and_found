4d1c6a9052d0b31523b049c235b60b8d581f5293
Adrian Yu <adrian.yu@outlook.com>
2025-04-03 09:22:46 +0800

    image fixed

--- Frontend/src/pages/BusLink.jsx
+++ Frontend/src/pages/BusLink.jsx
@@ -8,7 +8,7 @@ function BusLink() {
             <section>
                 <h1 className="lfh1">Bus Information</h1>
                 <div className="bus-content">
-                    <p>I like bananas</p>
+                    <p>If you lost something on the bus, and it isn't at the main office, please contact the bus company directly. Thank you!</p>
                     <a href="https://www.kcbh.com.hk/local_nonfranchised.php?lang=en" target="_blank" rel="noopener noreferrer" className="bus-link">
                         Visit Bus Service Website
                     </a>
