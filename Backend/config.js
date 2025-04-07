381870f9e489b36674a2dfc9751d53c037cd1d4c
Nathan Wang <125353357+cobalt5@users.noreply.github.com>
2025-04-07 15:19:29 +0800

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'your-project-url'
-const supabaseKey = 'your-anon-key'
+const supabaseUrl = 'https://pxnluqzebgytvroxwedj.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4bmx1cXplYmd5dHZyb3h3ZWRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2NDA3NzEsImV4cCI6MjA1OTIxNjc3MX0.6SW7U4q4HY5KbjoqUQ96fQhsNlQtUGl8PGR_rUMlkOI'
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
 export const PORT = process.env.PORT || 8000;
 
-export const mongoURL ="mongodb+srv://root:SNzJheXm5ObQeQrh@lostandfound.toc33hc.mongodb.net/lost-items?retryWrites=true&w=majority";
+// export const mongoURL ="mongodb+srv://root:SNzJheXm5ObQeQrh@lostandfound.toc33hc.mongodb.net/lost-items?retryWrites=true&w=majority";
