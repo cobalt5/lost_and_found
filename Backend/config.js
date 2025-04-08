611fc3476696d5578b6b7bc8a1a1c4aa927aacac
Nathan Wang <125353357+cobalt5@users.noreply.github.com>
2025-04-08 14:03:27 +0800

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://pxnluqzebgytvroxwedj.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4bmx1cXplYmd5dHZyb3h3ZWRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2NDA3NzEsImV4cCI6MjA1OTIxNjc3MX0.6SW7U4q4HY5KbjoqUQ96fQhsNlQtUGl8PGR_rUMlkOI'
+const supabaseUrl =  process.env.SUPABASE_URL || 'https://pxnluqzebgytvrowhedj.supabase.co'
+const supabaseKey = process.env.SUPABSE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4bmx1cXplYmd5dHZyb3h3ZWRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2NDA3NzEsImV4cCI6MjA1OTIxNjc3MX0.6SW7U4q4HY5KbjoqUQ96fQhsNlQtUGl8PGR_rUMlkOI'
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
 export const PORT = process.env.PORT || 8000;
